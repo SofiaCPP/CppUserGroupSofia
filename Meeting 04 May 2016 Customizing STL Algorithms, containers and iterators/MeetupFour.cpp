c2cd4593e105725fd3a0265fbe30b9d832ba0fd7
Dimitar Mirchev <dimitko8@yahoo.com>
2016-05-15 09:04:16 +0300

    Meetup 4 presentation and source code

---
+++ Meeting 04 May 2016 Customizing STL Algorithms, containers and iterators/MeetupFour.cpp
@@ -0,0 +1,150 @@
+// MeetupFour.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <vector>
+#include <list>
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <numeric>
+#include <functional>
+
+// #include <boost/algorithm/cxx11/copy_if.hpp>
+
+#include "meetingfour.h"
+
+void Test_ContPIterator()
+{
+	ContPIterator<int, 10> container;
+
+	int n = 0;
+	std::generate(container.begin(), container.end(), [&n] { return n++; });
+
+	PrintContainer(container);
+
+	std::transform(container.begin(), container.end(), container.begin(),
+		[](int& c) { return c + 5; });
+
+	std::sort(container.begin(), container.end(), std::greater<>());
+
+	PrintContainer(container);
+}
+
+void Test_ContInsert()
+{
+	ContInsert<int> container;
+
+	std::fill_n(std::inserter<ContInsert<int>>(container, 0), 5, 1);
+
+	PrintContainer(container);
+
+	std::transform(container.begin(), container.end(), container.begin(),
+		[](int& n) { return n * 5 + (RandomBool() ? 5 : 10 ); });
+
+	PrintContainer(container);
+}
+
+void Test_ContPushBack()
+{
+	ContPushBack<int> container;
+
+	std::vector<int> v(20);
+	int n = 0;
+	std::generate(v.begin(), v.end(), [&n] { return n++; });
+
+	std::copy_if(v.begin(), v.end(),
+		std::back_inserter<ContPushBack<int>>(container),
+		[](int n) { return n % 2 == 0; });
+
+	PrintContainer(container);
+
+	std::for_each(container.begin(), container.end(),
+								[](int& n) { n = n % 3; });
+
+	PrintContainer(container);
+}
+
+void Test_WierdContainer()
+{
+	WierdContainer<int> container;
+
+	// add 5 ones
+	std::fill_n(std::inserter<WierdContainer<int>>(container, container.begin()), 5, 1);
+
+	PrintContainer(container);
+
+	// add 0 to 19 in a vector
+	std::vector<int> v(20);	int n = 0;
+	std::generate(v.begin(), v.end(), [&n] { return n++; });
+
+	// copy to container only the even ones
+	std::copy_if(v.begin(), v.end(), std::inserter<WierdContainer<int>>(container, container.begin()), [](int n) { return n % 2 == 0; });
+
+	PrintContainer(container);
+
+	// add 5 to each element
+	std::transform(container.begin(), container.end(), container.begin(),
+		[](int& c) { return c + 5; });
+
+	PrintContainer(container);
+
+	// check if all are positive
+	bool isPositive = std::any_of(container.begin(), container.end(), [](auto& n) { return n > 0; });
+
+	// slide them forward by 1 element
+	std::rotate(container.begin(), ++container.begin(), container.end());
+
+	PrintContainer(container);
+
+	// sum all elements
+	int sum = std::accumulate(container.begin(), container.end(), 0);
+	std::cout << sum << '\n';
+
+	// find first adjesten that first is greater than second
+	auto it = std::adjacent_find(container.begin(), container.end(), std::greater<int>());
+
+	std::cout << *it << '\n';
+	WierdContainer<int> container2;
+
+	// use boost! algorithm copy until the element that is equal to 17
+	//boost::algorithm::copy_until(container.begin(), container.end(), std::inserter<WierdContainer<int>>(container2, container2.begin()), [](int i) { return i == 17; });
+	//PrintContainer(container2);
+}
+
+void Test_Algorithm() 
+{
+	std::vector<int> v({ 1,1,1,1,1,2,1,1,2,1,1 });
+
+	// { 1,1,1,1,1,2,1,1,2,1,1 }
+	bool bIsVector1 = is_every_D_after_first_occurrence(v.begin(), v.end(), 3, 2);	// true
+	// { 1,1,1,1,1,2,1,1,2,1,1 }
+	bool bIsVector2 = is_every_D_after_first_occurrence(v.begin(), v.end(), 3, 1);	// true
+
+	std::list<int> l({ 1,1,2,1,1,2,1,1,2,1,1,3 });
+	// { 1,1,2,1,1,2,1,1,2,1,1,3 }
+	bool bIsList1 = is_every_D_after_first_occurrence(l.begin(), l.end(), 3, 2);	// false
+	// { 1,1,2,1,1,2,1,1,2,1,1,3 }
+	bool bIsList2 = is_every_D_after_first_occurrence(l.begin(), l.end(), 3, 1);	// true
+}
+
+
+int main()
+{
+	std::cout << "\n Test_ContPIterator\n\n";
+	Test_ContPIterator();
+	std::cout << "\n Test_ContInsert \n\n";
+	Test_ContInsert();
+	std::cout << "\n Test_ContPushBack \n\n";
+	Test_ContPushBack();
+	std::cout << "\n Test_WierdContainer  \n\n";
+	Test_WierdContainer();
+	std::cout << "\n Test_Algorithm  \n\n";
+	Test_Algorithm();
+
+	return 0;
+}
+
---
+++ Meeting 04 May 2016 Customizing STL Algorithms, containers and iterators/meetingfour.h
@@ -0,0 +1,317 @@
+#pragma once
+
+// Help functions 
+
+// returning true or false 
+inline bool RandomBool()
+{
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	static std::bernoulli_distribution d(0.50);
+
+	return d(gen);
+}
+
+// prints the container
+template<class T> void PrintContainer(const T& container)
+{
+	for (const auto& i : container)
+		std::cout << i << ' ';
+
+	std::cout << '\n';
+}
+
+
+// Simple container with pointers as iterators
+template<class T, int N = 100>
+class ContPIterator
+{
+	T data[N];
+
+public:
+	auto begin() { return &data[0]; }
+	auto end() { return &data[N]; }	 // we never write here
+
+	auto begin() const { return &data[0]; }
+	auto end() const { return &data[N]; }	// we never write here
+
+};
+
+template<class T>
+struct ContInsert {
+
+	using iterator = int;
+	using value_type = T;
+
+	iterator insert(iterator, const T&  value)
+	{
+		m_Vect.push_back(value);
+		return 0;
+	}
+	auto begin()  { return m_Vect.begin(); }
+	auto end()  { return m_Vect.end(); }
+
+	auto begin() const { return m_Vect.cbegin(); }
+	auto end() const { return m_Vect.cend(); }
+
+private:
+	std::vector<T> m_Vect;
+};
+
+template<class T>
+struct ContPushBack {
+
+	using value_type = T;
+
+	void push_back(const T& value)
+	{
+		m_Vect.push_back(value);
+	}
+
+	auto begin() { return m_Vect.begin(); }
+	auto end() { return m_Vect.end(); }
+
+	auto begin() const { return m_Vect.cbegin(); }
+	auto end() const { return m_Vect.cend(); }
+private:
+	std::vector<T> m_Vect;
+};
+
+template<class T>
+class WierdNode {
+	std::unique_ptr<WierdNode<T>> left;
+	std::unique_ptr<WierdNode<T>> right;
+	T value;
+	WierdNode<T>* parent;
+
+public:
+	WierdNode(const T& v, WierdNode<T>* p) : value(v), parent(p) {}
+	T& Value() { return value; }
+
+	void insert(const T& value)
+	{
+		if (RandomBool())
+		{
+			if (left)
+				left->insert(value);
+			else
+				left = std::make_unique<WierdNode<T>>(value, this);
+		}
+		else
+		{
+			if (right)
+				right->insert(value);
+			else
+				right = std::make_unique<WierdNode<T>>(value, this);
+		}
+	}
+
+	WierdNode<T>* next()
+	{
+		if (left)
+			return left.get();
+
+		if (right)
+			return right.get();
+			
+		return parent->next(this);
+	}
+
+private:
+
+	WierdNode<T>* next(WierdNode<T>* child)
+	{
+		if (left.get() == child)
+			if (right)
+				return right.get();
+
+		if (parent == nullptr)
+			return nullptr;
+		else
+			return parent->next(this);
+	}
+
+};
+
+template<class Container>
+class WierdIterator : public std::iterator<std::forward_iterator_tag, typename Container::value_type>
+{
+public:
+	using value_type = typename Container::value_type;
+	using  _Myiter  = WierdIterator<Container>;
+	using  _Nodeptr = typename Container::_Nodeptr;
+
+	WierdIterator()
+		: _Ptr(nullptr)
+	{	// construct with null node pointer
+	}
+
+	WierdIterator(_Nodeptr _Pnode)
+		: _Ptr(_Pnode)
+	{
+	}
+
+	value_type& operator*() 
+	{	// return designated value
+		return _Ptr->Value();
+	}
+
+	value_type* operator->() 
+	{	// return pointer to class object
+		return &_Ptr->Value();
+	}
+
+	_Myiter& operator++()
+	{	// preincrement
+		_Ptr = _Ptr != nullptr ? _Ptr->next() : nullptr;
+		return (*this);
+	}
+
+	_Myiter operator++(int)
+	{	// postincrement
+		_Myiter _Tmp = *this;
+		++*this;
+		return (_Tmp);
+	}
+
+	bool operator==(const _Myiter& _Right) const
+	{	// test for iterator equality
+		return (_Ptr == _Right._Ptr);
+	}
+
+	bool operator!=(const _Myiter& _Right) const
+	{	// test for iterator inequality
+		return (!(*this == _Right));
+	}
+
+	_Nodeptr _Ptr;	// pointer to node
+};
+
+template<class T>
+class WierdContainer {
+
+	std::unique_ptr<WierdNode<T>> root;
+
+public:
+	using value_type = T;
+	using iterator = WierdIterator<WierdContainer<T>>;
+	using _Nodeptr = WierdNode<T>*;
+
+	iterator insert(iterator, const T&  value)
+	{
+		if (root)
+			root->insert(value);
+		else
+		{
+			root = std::make_unique<WierdNode<T>>(value, nullptr);
+		}
+
+		return iterator();
+	}
+ 
+	iterator begin() const 
+	{
+		return iterator(root.get());
+	}
+
+	iterator end() const 
+	{
+		return iterator();
+	}
+};
+
+template <class Iter>
+bool is_every_D_impl(std::random_access_iterator_tag, Iter _First, Iter _Last, size_t D)
+{
+	if (_First == _Last)
+		return false;
+
+	auto N = *_First;
+	do
+	{
+		if (_Last - _First <= D)
+			return true;
+
+		_First += D; 		
+
+		if (N != *_First)
+			return false;
+
+	} while (true);
+}
+
+template <class Iter>
+bool is_every_D_impl(std::forward_iterator_tag, Iter _First, Iter _Last, size_t D)
+{
+	auto N = *_First;
+	do
+	{
+		if (_First == _Last)
+			return false;
+
+		for (auto i = 0; i < D; ++i)
+			if (++_First == _Last)
+				return true;
+
+		if (N != *_First)
+			return false;
+
+	} while (true);
+}
+
+template <class Iter>
+bool is_every_D(Iter _First, Iter _Last, size_t D)
+{
+	return is_every_D_impl(typename std::iterator_traits<Iter>::iterator_category(), _First, _Last, D);
+}
+
+//template <class Iter>
+//bool is_every_D(Iter _First, Iter _Last, size_t D)
+//{
+//	if (_First == _Last)
+//		return false;
+//
+//	auto N = *_First;
+//	do
+//	{
+//		for (auto i = 0; i < D; ++i)
+//			if (++_First == _Last)
+//				return true;
+//
+//		if (N != *_First)
+//			return false;
+//
+//	} while (true);
+//}
+//
+
+template <class Iter, class Type>
+bool is_every_D_after_first_occurrence(Iter _First, Iter _Last, size_t D, Type N)
+{
+	auto _Found = std::find(_First, _Last, N);
+
+	return is_every_D(_Found, _Last, D);
+}	
+
+
+//template <class Iter, class Type>
+//bool is_every_D_after_first_occurrence(Iter _First, Iter _Last, size_t D, Type N)
+//{
+//	auto _Found = std::find(_First, _Last, N);
+//
+//	if (_First == _Last)
+//		return false;
+//
+//	do 
+//	{
+//		for (auto i = 0; i < D; ++i)
+//			if (++_Found == _Last)
+//				return true;
+//
+//		if (N != *_Found)
+//			return false;
+//
+//	} while (true);
+//}
+//
+
